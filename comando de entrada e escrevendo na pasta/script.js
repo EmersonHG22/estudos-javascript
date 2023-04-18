fd3dea9f29eba2c360dbe68940dd23fb0be7f189
Emerson Gomes <105255445+EmersonHG22@users.noreply.github.com>
2023-04-17 21:22:52 -0300

    subindo estudos

---
+++ comando de entrada e escrevendo na pasta/script.js
@@ -0,0 +1,9 @@
+var nome = prompt("qual é seu nome?");
+
+var sobrenome = prompt ("Qual é o seu sobrenome?");
+
+console.log("Olá" + nome + " " + sobrenome);
+
+document.write("<h1> Bem vindo "  + nome +  "</h1>");
+
+document.write("<img src='https://sujeitoprogramador.com/steve.png'>")
